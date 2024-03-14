dac783bd700f3e45a33c6f2d401614f57d821ea8
ginayawa <dsawayanagi@icloud.com>
2024-03-15 08:30:30 +0900

    no message

--- archive/myTypeScript/my-app/_app/db_control/create_tables.py
+++
@@ -1,8 +0,0 @@
-import platform
-print(platform.uname())
-
-from db_control.mymodels import Base #, User, Comment
-from db_control.connect import engine
-
-print("Creating tables >>> ")
-Base.metadata.create_all(bind=engine)
